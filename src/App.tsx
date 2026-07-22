c93c33fb5a3b10feebc3d1ecfa371377863a3913
nicolasey <nicolas.sey@gmail.com>
2026-07-22 12:07:33 +0200

    add app area

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Route, Routes } from "react-router"
-import { Home } from "@/modules/home/pages"
-import { NotFound } from "@/modules/errors/pages"
-import { Login } from "@/modules/auth/pages"
+import { lazy } from "react"
+import { Route, Routes } from "react-router";
+import AppLayout from "./components/ds/layouts/app.layout";
+
+const Home = lazy(() => import("@/modules/home/pages/Home"))
+const NotFound = lazy(() => import("@/modules/errors/pages/NotFound"))
+const Login = lazy(() => import("@/modules/auth/pages/Login"))
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+
+      <Route path="app" element={<AppLayout />}>
+        <Route index element={<Home />} />
+      </Route>
 
       <Route path="*" element={<NotFound />} />
     </Routes>

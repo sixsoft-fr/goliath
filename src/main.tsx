882d11c1d5085da670486fd3b2260bc6043f20c2
nicolasey <nicolas.sey@gmail.com>
2026-07-23 15:53:08 +0200

    add global suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from "react"
+import { StrictMode, Suspense } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import "@/lib/i18n"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { Spinner } from "@/components/ui/spinner"
 import { BrowserRouter } from "react-router"
 import { QueryProvider } from "@/lib/query"
 import { AuthProvider } from "@/modules/auth/auth.context"
@@ -15,7 +17,9 @@ createRoot(document.getElementById("root")!).render(
         <AuthProvider>
           <ThemeProvider>
             <TooltipProvider>
-              <App />
+              <Suspense fallback={<Spinner className="size-8 w-full" />}>
+                <App />
+              </Suspense>
             </TooltipProvider>
           </ThemeProvider>
         </AuthProvider>

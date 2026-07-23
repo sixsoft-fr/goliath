7aba1c40be4bd11660b68f4cca9e35cc559d1c2d
nicolasey <nicolas.sey@gmail.com>
2026-07-23 11:00:42 +0200

    complete user type

--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -1,7 +1,15 @@
-export type User = {
-  id: string
+import type { Model } from "@/modules/core"
+
+export type User = Model & {
   email: string
+  emailVerifiedAt: Date | null
+
   name?: string
+  locale: string;
+  avatar: string | null;
+  slug: string
+
+  account_id: number;
 }
 
 export type AuthSession = {

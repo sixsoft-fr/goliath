26f402054d9d08b846f241f65dec618f16295bcb
nicolasey <nicolas.sey@gmail.com>
2026-08-07 12:46:10 +0200

    extend admin conf

--- src/modules/sidebar/sidebar.config.tsx
+++ src/modules/sidebar/sidebar.config.tsx
@@ -1,6 +1,6 @@
 import type { SidebarItem } from "./sidebar.types"
 import { HugeiconsIcon } from "@hugeicons/react"
-import { Users } from "@hugeicons/core-free-icons"
+import { Users, ShoppingCart } from "@hugeicons/core-free-icons"
 
 export type SidebarConfig = {
   admin: SidebarItem[]
@@ -18,6 +18,23 @@ export const sidebarConfig: SidebarConfig = {
           title: "sidebar.admin.users",
           icon: <HugeiconsIcon icon={Users} strokeWidth={2} />,
           url: "/admin/users",
+        },
+      ]
+    },
+    {
+      title: "sidebar.admin.products-menu",
+      url: "#",
+      icon: <HugeiconsIcon icon={ShoppingCart} strokeWidth={2} />,
+      items: [
+        {
+          title: "sidebar.admin.products",
+          url: "/app/products",
+          icon: <HugeiconsIcon icon={ShoppingCart} strokeWidth={2} />,
+        },
+        {
+          title: "sidebar.admin.referentials",
+          url: "/app/referentials",
+          icon: <HugeiconsIcon icon={ShoppingCart} strokeWidth={2} />,
         },
       ]
     }
